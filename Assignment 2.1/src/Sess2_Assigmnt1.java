0f1acc2bd51ad8b2a674120659a7875a2b656c39
Adarsh Gupta <adarshgupta139@gmail.com>
2016-12-18 17:25:45 +0530

    Added Source Code

---
+++ Assignment 2.1/src/Sess2_Assigmnt1.java
@@ -0,0 +1,13 @@
+public class Sess2_Assigmnt1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		if(System.out.printf("Name : Adarsh") == null){}
+	}
+
+}
+/*
+******************************************************************************
+Name : Adarsh
+******************************************************************************
+*/
